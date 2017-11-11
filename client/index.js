044de7bca43dc2833dcf5cd6102326d29e6ed17d
Thikondrius <robin.lebhar@gmail.com>
2017-11-11 18:21:46 -0500

    apollo provider

--- client/index.js
+++ client/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient ({});
 
 const Root = () => {
-  return <h1>Bienvenue sur Graphql Movie ;)</h1>
+  return (
+  <ApolloProvider client={client}>
+   <h1>Bienvenue sur Graphql Movie ;)</h1>
+  </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
