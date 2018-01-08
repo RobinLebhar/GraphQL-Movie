0a2cb9929b71945b5d2031ed46c53e52ed2679f7
Thikondrius <robin.lebhar@gmail.com>
2018-01-08 17:14:36 -0500

    creation du form et refacto

--- client/components/movie-create.js
+++ client/components/movie-create.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
 class MovieCreate extends Component {
+    constructor(props){
+        super(props);
+        this.state = { terms : ""};
+    }
   render() {
     return (
       <div>
-        Movie create
+        <h1>Ajouter un film</h1>
+        <form onSubmit={this.handleSubmit.bind(this)} className="input-field col s6">
+        <input 
+        type="text"
+        className="validate"
+        onChange={e => this.setState({terms : e.target.value})}
+        />
+        <label className="active">Titre</label>
+        </form>
       </div>
     )
+  }
+
+  handleSubmit(e){
+      console.log(this.state.terms);
   }
 }
 
