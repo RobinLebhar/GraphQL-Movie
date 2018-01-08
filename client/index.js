e95791906684e32e5143a2ba3835abe6dfd0584e
Robin Lebhar <robin_lebhar@hotmail.fr>
2018-01-08 16:19:26 -0500

    Update index.js

--- client/index.js
+++ client/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom';
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import MovieList from "./components/movie-list";
-import MovieCreate from "./components/movie-create";
-import { Router,  Route, hashHistory, IndexRedirect } from "react-router";
 const client = new ApolloClient ({});
 
 const Root = () => {
   return (
   <ApolloProvider client={client}>
-  <Router history={hashHistory}>
-    <Route path="/">
-    <IndexRedirect to="movies"/>
-    <Route path="movies" component={MovieList}/>
-    <Route path="movies/create" component={MovieCreate}/>
-    </Route>
-  </Router> 
+    <MovieList/>
   </ApolloProvider>
   )
 };
