013efc0f4a8b7e991478e6da64e418d5860ae3b3
Thikondrius <robin.lebhar@gmail.com>
2018-01-08 16:16:43 -0500

    react-router

---
+++ client/components/movie-create.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MovieCreate extends Component {
+  render() {
+    return (
+      <div>
+        Movie create
+      </div>
+    )
+  }
+}
+
+export default MovieCreate;
